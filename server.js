04039a93611ccf7896da855d0e54873678f5819d
Natanev92 <nvillasenor1@icloud.com>
2021-07-21 12:30:48 -0700

    Jokes_api

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+require("./server/config/mongoose.config");
+// This sends our mongoose.connect statement to initialize our database connection
+
+app.use(express.json(), express.urlencoded({ extended: true }));
+
+// We import the jokes routes function from our joke.routes.js file
+const AllMyJokeRoutes = require("./server/routes/joke.routes");
+AllMyJokeRoutes(app);
+
+app.listen(8000, () => console.log("The server is ready on port 8000"));
