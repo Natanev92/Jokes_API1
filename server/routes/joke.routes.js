bb786d94d2b2dc3535181f7a2f28b11204e4bc8a
Natanev92 <nvillasenor1@icloud.com>
2021-07-21 14:44:12 -0700

    Joker_api

--- server/routes/joke.routes.js
+++ server/routes/joke.routes.js
@@ -2,8 +2,9 @@ const JokeController = require("../controllers/joke.controller");
 // imported JokeController and created the express routes
 module.exports = app => {
   app.get("/api/jokes/", JokeController.findAllJokes);
+  app.post("/api/jokes/new", JokeController.createNewJoke);
   app.get("/api/jokes/:id", JokeController.findOneJoke);
   app.put("/api/jokes/update/:id", JokeController.updateExistingJoke);
-  app.post("/api/jokes/new", JokeController.createNewJoke);
   app.delete("/api/jokes/delete/:id", JokeController.deleteAnExistingJoke);
 };
+//url naming Conventions in cascading manner
